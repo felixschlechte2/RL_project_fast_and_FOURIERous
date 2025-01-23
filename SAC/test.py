539c369d47a480f5588fe88e40398f0969d7fa81
Felix Schlechte <felix.schlechte@t-online.de>
2025-01-23 14:18:41 +0100

    added test von sac

--- SAC/test.py
+++ SAC/test.py
@@ -4,50 +4,42 @@ import numpy as np
 # import hockey.hockey_env as h_env
 import torch
 import core
+# from sac import *
 
 env = "Pendulum-v1"
-env = gym.make(env)
+env = gym.make(env, render_mode="human")
 
 ac = core.MLPActorCritic(env.observation_space, env.action_space)
 
 # models_dir = r".\Reinforcement Learning\ExcercisesGitHub\exercises mit venv\project_code\models\TD3"
-models_path = r".\Reinforcement Learning\ExcercisesGitHub\exercises mit venv\project_code\models\SAC\results\test.pth"
+# models_path = r".\Reinforcement Learning\ExcercisesGitHub\exercises mit venv\project_code\models\SAC\results\SAC_pendulum_30.pth"
+models_path = r".\results2\SAC_pendulum_5.pth"
 
-ac.load_state_dict(torch.load(models_path, weights_only=False))
-
-o, i = env.reset()
-
-print(o)
+ac.load_state_dict(torch.load(models_path, weights_only=False, map_location=torch.device('cpu')))
 
 def get_action(o, deterministic=False):
         return ac.act(torch.as_tensor(o, dtype=torch.float32), 
                       deterministic)
 
-o, r, d, _, _ = env.step(get_action(o))
+obs, info = env.reset()
 
-# obs, info = env.reset()
-# obs_agent2 = env.obs_agent_two()
-
-# # print(obs)
-# # print(obs_agent2)
-
-# # env.render(mode="human")
+print(ac.pi.net(torch.tensor(obs)))
+print(ac.pi.mu_layer.weight)
+print(ac.pi.mu_layer(ac.pi.net(torch.tensor(obs))))
+pi_action = ac.pi.mu_layer(ac.pi.net(torch.tensor(obs)))
+print(torch.tanh(pi_action))
+print(f"act limit: {ac.pi.act_limit}")
+print(ac.pi.act_limit * torch.tanh(pi_action))
+print(get_action(obs))
 
 # for ep in range(5):
 #     obs, info = env.reset()
 #     d = False
 #     while not d:
-#         env.render(mode="human")
-#         action_agent = ddpg.act(obs)[:4]
-#         action_opp = basic_opp.act(obs_agent2)
-#         # action_agent = np.array(action_agent)
-#         # action_random = np.array(env.action_space.sample()[:4])
-#         action = np.hstack((action_agent, action_opp))
-#         # obs, r, d, t, info = env.step(action_agent)
-#         obs, r, d, t, info = env.step(action)
-#         # print(r)
-#         obs_agent2 = env.obs_agent_two()
-
+#         env.render()
+#         action_agent = get_action(obs)
+#         obs, r, d, t, info = env.step(action_agent)
+#         print(action_agent)
 
 # env.close()
 
