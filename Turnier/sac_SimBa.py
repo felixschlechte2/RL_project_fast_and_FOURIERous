5cbd51696fc2daef4bbcf2d2bc6a773294d85ff6
lennartgoldscheider <l.goldscheider@outlook.de>
2025-02-23 14:39:01 +0100

    added files for competition selection

---
+++ Turnier/sac_SimBa.py
@@ -0,0 +1,217 @@
+import os
+import torch
+import torch.nn.functional as F
+from torch.optim import Adam
+from utils import soft_update, hard_update
+import NN_SimBa as models
+import numpy as np
+import time
+import pickle as pkl
+
+# class statistics():
+#     def __init__(self, attributes, saving_path):
+#         self.stats = {}
+#         for attr in attributes:
+#             self.stats[attr] = []
+#         self.saving_path = saving_path
+
+#     def save_data(self, attributes, values):
+#         if len(attributes) != len(values):
+#             log("stats saving error: both list must be of equal length")
+#             return
+#         else:
+#             for i in range(len(attributes)):
+#                 self.stats[attributes[i]].append(values[i])
+#     def save_statistics(self):
+#         log("save statistics")
+#         with open(self.saving_path, "wb") as f:
+#             pkl.dump(self.stats,f)
+#     def load_statistics(self,path):
+#         print("load statistics")
+#         with open(path,"rb") as f:
+#             self.stats = pkl.load(f)
+#     def return_values(self,attribute):
+#         return self.stats[attribute]
+
+
+def log(log):
+    log_file = f"./log_sac.txt"
+    with open(log_file, "a") as file:
+        file.write(log)
+        file.write("\n")
+
+class SAC(object):
+    def __init__(self, num_inputs, action_space,num_blocks,actor, critic, args):
+
+        #self.stats = statistics(['log_pi_mean','log_pi_std'], fr"./statistics/stats_test_{suffix}.pkl")
+
+        # self.gamma = args.gamma
+        # self.tau = args.tau
+        # self.alpha = args.alpha
+
+        # self.policy_type = args.policy
+        # self.target_update_interval = args.target_update_interval
+        # self.automatic_entropy_tuning = args.automatic_entropy_tuning
+
+        # self.device = torch.device("cuda" if args.cuda else "cpu")
+        # #log(str(self.device))
+
+        # #self.critic = models.SACClippedDoubleCritic(dim = num_inputs + action_space.shape[0],block_type = critic, num_blocks = num_blocks, hidden_dim = args.hidden_size).to(device=self.device) #here
+        # # print(self.critic)
+        # self.critic = QNetwork(num_inputs, action_space.shape[0], args.hidden_size).to(device=self.device)
+        # # print(self.critic)
+        # # time.sleep(20)
+
+        # self.critic_optim = Adam(self.critic.parameters(), lr=args.lr)
+
+        # #self.critic_target = models.SACClippedDoubleCritic(dim = num_inputs + action_space.shape[0],block_type = critic, num_blocks = num_blocks, hidden_dim = args.hidden_size).to(device=self.device) #here
+        # self.critic_target = QNetwork(num_inputs, action_space.shape[0], args.hidden_size).to(device=self.device)
+        
+        # hard_update(self.critic_target, self.critic)
+
+        # #self.policy = models.SACActor(dim=num_inputs, block_type=actor, num_blocks=num_blocks, hidden_dim= args.hidden_size, action_dim=action_space.shape[0],action_space = action_space).to(device=self.device)
+        # #self.policy_optim = Adam(self.policy.parameters(), lr=args.lr)
+
+        # if self.automatic_entropy_tuning is True:
+        #     self.target_entropy = -torch.prod(torch.Tensor(action_space.shape).to(self.device)).item()
+        #     self.log_alpha = torch.zeros(1, requires_grad=True, device=self.device)
+        #     self.alpha_optim = Adam([self.log_alpha], lr=args.lr)
+
+        # self.policy = GaussianPolicy(num_inputs, action_space.shape[0], args.hidden_size, action_space).to(self.device)
+        # self.policy_optim = Adam(self.policy.parameters(), lr=args.lr)
+
+        self.gamma = args.gamma
+        self.tau = args.tau
+        self.alpha = args.alpha
+
+        self.policy_type = args.policy
+        self.target_update_interval = args.target_update_interval
+        self.automatic_entropy_tuning = args.automatic_entropy_tuning
+
+        self.device = torch.device("cuda" if args.cuda else "cpu")
+        log(str(self.device))
+
+        #self.critic = QNetwork(num_inputs, action_space.shape[0], args.hidden_size).to(device=self.device)
+        self.critic = models.SACClippedDoubleCritic(dim = num_inputs + action_space.shape[0],block_type = critic, num_blocks = num_blocks, hidden_dim = args.hidden_size).to(device=self.device) #here
+        self.critic_optim = Adam(self.critic.parameters(), lr=args.lr)
+
+        #self.critic_target = QNetwork(num_inputs, action_space.shape[0], args.hidden_size).to(self.device) 
+        self.critic_target = models.SACClippedDoubleCritic(dim = num_inputs + action_space.shape[0],block_type = critic, num_blocks = num_blocks, hidden_dim = args.hidden_size).to(device=self.device) #here
+        hard_update(self.critic_target, self.critic)
+
+        # if self.policy_type == "Gaussian":
+        #     # Target Entropy = −dim(A) (e.g. , -6 for HalfCheetah-v2) as given in the paper
+        if self.automatic_entropy_tuning is True:
+            self.target_entropy = -torch.prod(torch.Tensor(action_space.shape).to(self.device)).item()
+            self.log_alpha = torch.zeros(1, requires_grad=True, device=self.device)
+            self.alpha_optim = Adam([self.log_alpha], lr=args.lr)
+
+        #     self.policy = GaussianPolicy(num_inputs, action_space.shape[0], args.hidden_size, action_space).to(self.device)
+        #     self.policy_optim = Adam(self.policy.parameters(), lr=args.lr)
+
+        self.policy = models.SACActor(dim=num_inputs, block_type=actor, num_blocks=num_blocks, hidden_dim= args.hidden_size, action_dim=action_space.shape[0],action_space = action_space).to(device=self.device)
+        self.policy_optim = Adam(self.policy.parameters(), lr=args.lr)
+        
+    def select_action(self, state, evaluate=False):
+        state = torch.FloatTensor(state).to(self.device).unsqueeze(0)
+        if evaluate is False:
+            action, _, _ = self.policy.sample(state)
+        else:
+            _, _, action = self.policy.sample(state)
+        return action.detach().cpu().numpy()[0]
+    
+   
+    #new here just updated inputs basically
+    def update_parameters(self, memory, batch_size, updates):
+        #log_pi_simba_mean = []
+        #log_pi_simba_std = []
+        states, actions, rewards, next_states, goals = memory.sample(batch_size)
+
+        inputs = np.concatenate([states, goals], axis=1)
+        next_inputs = np.concatenate([next_states, goals], axis=1)
+
+        inputs = torch.Tensor(inputs).to(self.device)
+        rewards = torch.Tensor(rewards).to(self.device)
+        next_inputs = torch.Tensor(next_inputs).to(self.device)
+        actions = torch.Tensor(actions).to(self.device)
+
+        with torch.no_grad():
+            next_state_action, next_state_log_pi, _ = self.policy.sample(next_inputs)
+            qf1_next_target, qf2_next_target = self.critic_target(next_inputs, next_state_action)
+            #target_q = self.critic_target(next_inputs, self.actor_target(next_inputs))
+            min_qf_next_target = torch.min(qf1_next_target, qf2_next_target) - self.alpha * next_state_log_pi
+            next_q_value = rewards + self.gamma * (min_qf_next_target) #here no mask
+            # = torch.clamp(target_returns, -1 / (1 - self.gamma), 0)
+
+        qf1, qf2 = self.critic(inputs, actions) # Two Q-functions to mitigate positive bias in the policy improvement step
+        qf1_loss = F.mse_loss(qf1, next_q_value)  # JQ = 𝔼(st,at)~D[0.5(Q1(st,at) - r(st,at) - γ(𝔼st+1~p[V(st+1)]))^2]
+        qf2_loss = F.mse_loss(qf2, next_q_value)  # JQ = 𝔼(st,at)~D[0.5(Q1(st,at) - r(st,at) - γ(𝔼st+1~p[V(st+1)]))^2]
+        qf_loss = qf1_loss + qf2_loss
+        self.critic_optim.zero_grad()
+        qf_loss.backward()
+        self.critic_optim.step()
+
+        pi, log_pi, _ = self.policy.sample(inputs)
+        #log_pi_simba_mean.append(log_pi.mean().item())
+        #log_pi_simba_std.append(log_pi.std().item())
+        #self.stats.save_data(['log_pi_mean','log_pi_std'], [log_pi.mean().item(),log_pi.std().item()])
+        qf1_pi, qf2_pi = self.critic(inputs, pi)
+        min_qf_pi = torch.min(qf1_pi, qf2_pi)
+        policy_loss = ((self.alpha * log_pi) - min_qf_pi).mean() # Jπ = 𝔼st∼D,εt∼N[α * logπ(f(εt;st)|st) − Q(st,f(εt;st))]
+
+
+        self.policy_optim.zero_grad()
+        policy_loss.backward()
+        self.policy_optim.step()
+    
+        if self.automatic_entropy_tuning:
+            alpha_loss = -(self.log_alpha * (log_pi + self.target_entropy).detach()).mean()
+
+            self.alpha_optim.zero_grad()
+            alpha_loss.backward()
+            self.alpha_optim.step()
+
+            self.alpha = self.log_alpha.exp()
+            alpha_tlogs = self.alpha.clone() # For TensorboardX logs
+        else:
+            alpha_loss = torch.tensor(0.).to(self.device)
+            alpha_tlogs = torch.tensor(self.alpha) # For TensorboardX logs
+
+        if updates % self.target_update_interval == 0:
+            soft_update(self.critic_target, self.critic, self.tau)
+
+        return qf1_loss.item(), qf2_loss.item(), policy_loss.item(), alpha_loss.item(), alpha_tlogs.item(), log_pi.mean().item()
+
+
+    # Save model parameters
+    def save_checkpoint(self, suffix="", ckpt_path=None):
+        if not os.path.exists('checkpoints/'):
+            os.makedirs('checkpoints/')
+        if ckpt_path is None:
+            ckpt_path = "checkpoints/SAC_{}".format(suffix)
+        print('Saving models to {}'.format(ckpt_path))
+        torch.save({'policy_state_dict': self.policy.state_dict(),
+                    'critic_state_dict': self.critic.state_dict(),
+                    'critic_target_state_dict': self.critic_target.state_dict(),
+                    'critic_optimizer_state_dict': self.critic_optim.state_dict(),
+                    'policy_optimizer_state_dict': self.policy_optim.state_dict()}, ckpt_path)
+
+    # Load model parameters
+    def load_checkpoint(self, ckpt_path, evaluate=False):
+        print('Loading models from {}'.format(ckpt_path))
+        if ckpt_path is not None:
+            checkpoint = torch.load(ckpt_path, weights_only=True, map_location=torch.device('cpu')) # hier : added weights_only and map_location
+            self.policy.load_state_dict(checkpoint['policy_state_dict'])
+            self.critic.load_state_dict(checkpoint['critic_state_dict'])
+            self.critic_target.load_state_dict(checkpoint['critic_target_state_dict'])
+            self.critic_optim.load_state_dict(checkpoint['critic_optimizer_state_dict'])
+            self.policy_optim.load_state_dict(checkpoint['policy_optimizer_state_dict'])
+
+            if evaluate:
+                self.policy.eval()
+                self.critic.eval()
+                self.critic_target.eval()
+            else:
+                self.policy.train()
+                self.critic.train()
+                self.critic_target.train()
