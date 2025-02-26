b661b1ce94f0ca6225bec638fb810687b4910590
lennartgoldscheider <l.goldscheider@outlook.de>
2025-02-26 15:34:43 +0100

    Update replay_memory.py

--- final/SIMBA/replay_memory.py
+++ final/SIMBA/replay_memory.py
@@ -1,9 +1,8 @@
 import random
 import numpy as np # type: ignore
-import os  # hier 
-import pickle  # hier 
-from copy import deepcopy as dc #here
-import time #here
+import os   
+import pickle   
+from copy import deepcopy as dc
 
 class ReplayMemory:
     def __init__(self, capacity,k_future,env, seed):
@@ -12,26 +11,17 @@ class ReplayMemory:
         self.buffer = []
         self.position = 0
         self.env = env
-        self.future_p = 1 - (1. / (1 + k_future))
+        self.future_p = 1 - (1. / (1 + k_future)) # for HER
 
-    #here everything
-    #def push(self, episode_dict, action, reward, next_state, done, desired_goal):
-    #    if len(self.buffer) < self.capacity:
-    #        self.buffer.append(None)
-    #    self.buffer[self.position] = (episode_dict, action, reward, next_state, done)
-    #    self.position = (self.position + 1) % self.capacity
-    
-    #new here
+    # add whole episodes
     def push(self, episode_dict):
         self.buffer.append(episode_dict)
         if len(self.buffer) > self.capacity:
             self.buffer.pop(0)
-        #assert len(self.memory) <= self.capacity
 
     def sample(self, batch_size):
-        #batch = random.sample(self.buffer, batch_size)
-        #state, action, reward, next_state, done = map(np.stack, zip(*batch))
 
+        # pick random episodes and random time indices for them
         ep_indices = np.random.randint(0, len(self.buffer), batch_size)
         episode_lengths = [len(self.buffer[i]["next_state"]) for i in ep_indices]
         time_indices = [np.random.randint(0, episode_lengths[i]) for i in range(batch_size)]
@@ -46,8 +36,8 @@ class ReplayMemory:
         for episode, timestep in zip(ep_indices, time_indices):
             states.append(dc(self.buffer[episode]["state"][timestep]))
             actions.append(dc(self.buffer[episode]["action"][timestep]))
-            desired_goals.append(dc(self.buffer[episode]["desired_goal"])) #here different
-            next_achieved_goals.append(dc(self.buffer[episode]["achieved_goal"][timestep])) #here different from git
+            desired_goals.append(dc(self.buffer[episode]["desired_goal"])) 
+            next_achieved_goals.append(dc(self.buffer[episode]["achieved_goal"][timestep]))
             next_states.append(dc(self.buffer[episode]["next_state"][timestep]))
             reward.append(dc(self.buffer[episode]["reward"][timestep]))
     
@@ -58,51 +48,41 @@ class ReplayMemory:
         next_states = np.vstack(next_states)
         reward= np.vstack(reward)
 
+        # select random episodes that get changed for HER
         her_indices = np.where(np.random.uniform(size=batch_size) < self.future_p)
 
-        #for every epsiode, note good states for her, aka in last quarter enemy half
-        #future_candidates = []
+        # for every episode, consider all good time steps in which the puck is in the enemy quarter
         future_t =[]
         for i in range(len(ep_indices)):
+            # good achieved goals:
             candidate = [
                 t for t in range(time_indices[i] + 1, episode_lengths[i]) 
-                if self.buffer[ep_indices[i]]["achieved_goal"][t][0] > 1.85  #achieved goal is good
+                if self.buffer[ep_indices[i]]["achieved_goal"][t][0] > 1.85
             ]
-            #future_candidates.append(candidate)
+            # add random time steps if no good state was reached
             if len(candidate) >0:
                 future_t.append(np.random.choice(candidate))
             else:
-                #future_offset = np.random.randint(1, episode_lengths[i] - time_indices[i] + 1)
-                #future_t.append(np.minimum(time_indices[i] + future_offset, episode_lengths[i] - 1)) #so that we are for sure < episode_len
                 future_t.append(-1)
         future_t = np.array([int(x) for x in future_t])
 
-        #future_ag = np.array([self.buffer[ep]["achieved_goal"][ft] for ep, ft in zip(ep_indices[her_indices], future_t[her_indices])])
-        future_ag = np.array([ #here
+        # change desired_goal to the good one's that were achieved
+        future_ag = np.array([
             self.buffer[ep]["achieved_goal"][ft] if ft != -1 else desired_goals[idx]
             for idx, (ep, ft) in enumerate(zip(ep_indices[her_indices], future_t[her_indices]))
             ])
-        desired_goals[her_indices] = future_ag #change desired_goal to the one's we achieved
+        desired_goals[her_indices] = future_ag 
 
-        #here everything
+        # calculate distance of next position of the puck and the desired position of the puck
         distance = []
-        for sub1, sub2 in zip(next_achieved_goals[her_indices], desired_goals[her_indices]):  # Iterate over corresponding sub-arrays
+        for sub1, sub2 in zip(next_achieved_goals[her_indices], desired_goals[her_indices]):
             distance.append(max(abs(a - b) for a, b in zip(sub1, sub2)))
         
-        # Define a threshold for success (e.g., 5 cm)
-        success_threshold = 1 #here statt 0.1
-
-        # Reward function (sparse or dense)
-        #rewards = -distance  # Dense reward (closer is better)
-        
-        # If object reaches goal, give a large positive reward
+        # calculate new rewards based on distance
+        success_threshold = 1 
         rewards = np.array([float(-x) if abs(x)> success_threshold else 10 for x in distance])
-        
-        # or += ??
-        #reward = np.array(rewards)
-        reward[her_indices] = rewards[:,np.newaxis]#[her_indices]
-        #rewards = np.expand_dims(rewards, 1) #here
-        #reward = np.expand_dims(reward, 1) #here
+ 
+        reward[her_indices] = rewards[:,np.newaxis]
 
         return states, actions, reward, next_states, desired_goals
 
