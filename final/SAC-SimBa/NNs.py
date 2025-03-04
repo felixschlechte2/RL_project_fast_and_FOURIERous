946af3ad0e38b6fbbf17c10bc3e6384aefd3764f
lennartgoldscheider <l.goldscheider@outlook.de>
2025-03-04 11:22:28 +0100

    deleted comments

--- final/SAC-SimBa/NNs.py
+++ final/SAC-SimBa/NNs.py
@@ -8,8 +8,26 @@ def weights_init_(m):
     if isinstance(m, nn.Linear):
         torch.nn.init.xavier_uniform_(m.weight, gain=1)
         torch.nn.init.constant_(m.bias, 0)
+
+class MLPBlockActor(nn.Module):
+    def __init__(self,dim, hidden_dim):
+        super().__init__()
+        self.fc1 = nn.Linear(dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        self.layer_norm1 = nn.LayerNorm(hidden_dim)
+        
+        self.activation = nn.ReLU()
+        self.apply(weights_init_)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.layer_norm1(x)
+        x = self.activation(x)
+        x = self.fc2(x) 
+        x = self.activation(x)
+        return x
     
-class MLPBlock(nn.Module):
+class MLPBlockCritic(nn.Module):
     def __init__(self,dim, hidden_dim):
         super().__init__()
         self.fc1 = nn.Linear(dim, hidden_dim)
@@ -22,16 +40,16 @@ class MLPBlock(nn.Module):
 
     def forward(self, x):
         x = self.fc1(x)
-        x = self.layer_norm1(x)
+        x = self.layer_norm1(x) 
         x = self.activation(x)
         x = self.fc2(x) 
-        x = self.layer_norm2(x)
-        x = self.activation(x)
+        x = self.layer_norm2(x) 
+        x = self.activation(x) 
         return x
 
 
 class ResidualBlock(nn.Module):
-    def __init__(self, hidden_dim):
+    def __init__(self, hidden_dim, dtype=torch.float32):
         super().__init__()
         self.layernorm1 = nn.LayerNorm(hidden_dim)
         self.fc1 = nn.Linear(hidden_dim, hidden_dim)
@@ -50,7 +68,7 @@ class ResidualBlock(nn.Module):
 
 
 class SACEncoder(nn.Module):
-    def __init__(self,dim, block_type, num_blocks, hidden_dim):
+    def __init__(self,dim, block_type, num_blocks, hidden_dim, dtype=torch.float32):
         super().__init__()
         self.dim = dim
         self.block_type = block_type
@@ -58,12 +76,12 @@ class SACEncoder(nn.Module):
         self.hidden_dim = hidden_dim
 
         if block_type == "mlp":
-            self.layer = MLPBlock(dim,hidden_dim)
+            self.layer = MLPBlockActor(dim,hidden_dim)
 
         elif block_type == "residual":
             self.input_layer = nn.Linear(dim, hidden_dim)
             self.blocks = nn.ModuleList([
-                ResidualBlock(hidden_dim) 
+                ResidualBlock(hidden_dim, dtype=dtype) 
                 for _ in range(num_blocks)
             ])
             self.norm = nn.LayerNorm(hidden_dim)
@@ -77,12 +95,42 @@ class SACEncoder(nn.Module):
             x = self.input_layer(x)
             for block in self.blocks:
                 x = block(x)
-            #x = self.norm(x) 
+            #x = self.norm(x) #here
         return x
     
+class SACEncoderCritic(nn.Module):
+    def __init__(self,dim, block_type, num_blocks, hidden_dim, dtype=torch.float32):
+        super().__init__()
+        self.dim = dim
+        self.block_type = block_type
+        self.num_blocks = num_blocks
+        self.hidden_dim = hidden_dim
 
+        if block_type == "mlp":
+            self.layer = MLPBlockCritic(dim,hidden_dim)
+
+        elif block_type == "residual":
+            self.input_layer = nn.Linear(dim, hidden_dim)
+            self.blocks = nn.ModuleList([
+                ResidualBlock(hidden_dim, dtype=dtype) 
+                for _ in range(num_blocks)
+            ])
+            self.norm = nn.LayerNorm(hidden_dim)
+        
+        self.apply(weights_init_)
+
+    def forward(self, x):
+        if self.block_type == "mlp":
+            x = self.layer(x)
+        elif self.block_type == "residual":
+            x = self.input_layer(x)
+            for block in self.blocks:
+                x = block(x)
+            #x = self.norm(x) #here
+        return x
+    
 class NormalTanhPolicy(nn.Module):
-    def __init__(self, hidden_dim, action_dim, log_std_min=-20, log_std_max=2):
+    def __init__(self, hidden_dim, action_dim, dtype=torch.float32, log_std_min=-20, log_std_max=2):
         super().__init__()
         self.action_dim = action_dim
         self.fc_mean = nn.Linear(hidden_dim, action_dim)
@@ -96,22 +144,23 @@ class NormalTanhPolicy(nn.Module):
         mean = self.fc_mean(x)
         log_std = self.fc_log_std(x)
         log_std = torch.clamp(log_std, self.log_std_min, self.log_std_max)
-        #std = log_std.exp() * temperature
+        #std = log_std.exp() * temperature #here
         #dist = Normal(mean, std)
 
-        return  mean, log_std #dist 
+        return  mean, log_std #dist #here
 
 
 class SACActor(nn.Module):
-    def __init__(self,dim, block_type, num_blocks, hidden_dim, action_dim,action_space=None):
+    def __init__(self,dim, block_type, num_blocks, hidden_dim, action_dim, dtype=torch.float32,action_space=None):
         super().__init__()
         self.encoder = SACEncoder(
             dim = dim,
             block_type=block_type,
             num_blocks=num_blocks,
-            hidden_dim=hidden_dim
+            hidden_dim=hidden_dim,
+            dtype=dtype
         )
-        self.predictor = NormalTanhPolicy(hidden_dim, action_dim)
+        self.predictor = NormalTanhPolicy(hidden_dim, action_dim, dtype=dtype)
 
         self.apply(weights_init_)
 
@@ -138,7 +187,6 @@ class SACActor(nn.Module):
         y_t = torch.tanh(x_t)
         action = y_t * self.action_scale + self.action_bias
         log_prob = normal.log_prob(x_t)
-        
         log_prob -= torch.log(self.action_scale * (1 - y_t.pow(2)) + 1e-6)
         log_prob = log_prob.sum(1, keepdim=True)
         mean = torch.tanh(mean) * self.action_scale + self.action_bias
@@ -161,13 +209,14 @@ class LinearCritic(nn.Module):
     
 
 class SACCritic(nn.Module):
-    def __init__(self,dim, block_type, num_blocks, hidden_dim):
+    def __init__(self,dim, block_type, num_blocks, hidden_dim, dtype=torch.float32):
         super().__init__()
-        self.encoder = SACEncoder(
+        self.encoder = SACEncoderCritic(
             dim = dim,
             block_type=block_type,
             num_blocks=num_blocks,
-            hidden_dim=hidden_dim
+            hidden_dim=hidden_dim,
+            dtype=dtype
         )
         self.predictor = LinearCritic(hidden_dim)
 
@@ -180,10 +229,10 @@ class SACCritic(nn.Module):
         return q
     
 class SACClippedDoubleCritic(nn.Module):
-    def __init__(self,dim, block_type, num_blocks, hidden_dim, num_qs=2):
+    def __init__(self,dim, block_type, num_blocks, hidden_dim, dtype=torch.float32, num_qs=2):
         super().__init__()
         self.q_networks = nn.ModuleList([
-            SACCritic(dim,block_type, num_blocks, hidden_dim)
+            SACCritic(dim,block_type, num_blocks, hidden_dim, dtype=dtype)
             for _ in range(num_qs)
         ])
         self.apply(weights_init_)
@@ -202,4 +251,3 @@ class SACTemperature(nn.Module):
 
     def forward(self):
         return self.log_temp.exp()
-
